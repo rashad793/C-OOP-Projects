e0a1216ab4d0fde9b82a52989e49d7ddfcb921f2
Abdallah Rashad <112658619+rashad793@users.noreply.github.com>
2025-01-26 13:23:31 +0200

    Add files via upload

---
+++ Exercise 1-Library management system project/Exercise 1-Library management system project/LibraryCard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_1_Library_management_system_project
+{
+    internal class LibraryCard
+    {
+        public int CardNum { get; set; }
+    }
+}
